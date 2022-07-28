f4478736347a18ec0d94cbe25e0cc0dd601f2a29
Henrique Dummer <92610573+HenriqueDummer@users.noreply.github.com>
2022-07-28 12:32:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const pokemon_img = document.querySelector(".pokemon_img")
+const pokemon_name = document.querySelector(".name")
+const pokemon_id = document.querySelector(".id")
+const next = document.querySelector(".next")
+const previous = document.querySelector(".previous")
+const form = document.querySelector(".form")
+const input_search = document.querySelector(".input_search")
+let pokemon_selector = 1
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+    if(APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemon_id.innerHTML = ''
+    pokemon_name.innerHTML = "Pesquisando.."
+    
+    const data = await fetchPokemon(pokemon);
+
+    if(data){
+
+        pokemon_img.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+        pokemon_name.innerHTML = data["name"]
+        pokemon_id.innerHTML = data["id"]
+        pokemon_selector = data["id"]
+    } else{
+        pokemon_img.src = 'https://i.pinimg.com/originals/0c/b2/36/0cb236777f219f7ddb6d21a8ac7ff2fa.png'
+        pokemon_name.innerHTML = "Não Encontrado :("
+        pokemon_id.innerHTML = ''
+    }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    renderPokemon(input_search.value.toLowerCase())
+    input_search.value = ''
+})
+
+next.addEventListener("click", () => {
+    if(pokemon_selector < 649){
+        pokemon_selector += 1;
+        renderPokemon(pokemon_selector)
+    } else{
+        pokemon_selector = 1;
+        renderPokemon(pokemon_selector)
+    }
+})
+
+previous.addEventListener("click", () => {
+    if(pokemon_selector > 1){
+        pokemon_selector -= 1;
+        renderPokemon(pokemon_selector)
+    } else {
+        pokemon_selector = 649;
+        renderPokemon(pokemon_selector)
+    }
+})
+
+renderPokemon(pokemon_selector)
+
